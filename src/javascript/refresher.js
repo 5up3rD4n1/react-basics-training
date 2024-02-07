6489741a5db905f2e6a7538e6d9819b8d86a4a05
mjvemo <74282064+mjvemo@users.noreply.github.com>
2024-02-06 22:34:33 -0600

    Add Solutions to Refresher

--- src/javascript/refresher.js
+++ src/javascript/refresher.js
@@ -3,6 +3,11 @@
 const object1 = { item: { hello: 1 } };
 
 // Your code goes here
+// const hello = object1.item.hello;
+
+const {
+  item: { hello },
+} = object1;
 
 console.log(hello);
 
@@ -11,8 +16,11 @@ console.log(hello);
 const object2 = { result: "success" };
 
 // Your code goes here
+function addProp(obj, prop, value) {
+  return { ...obj, [prop]: value };
+}
 
-addProp(object2, "newProp", "propValue");
+console.log(addProp(object2, "newProp", "propValue"));
 
 // Array Methods
 // Filter all values that are bigger than 20
@@ -25,6 +33,10 @@ const items = [
   { value: 50 },
   { value: 30 },
 ];
+
+function filterBiggerThan20(items) {
+  return items.filter((item) => item.value > 20);
+}
 
 console.log(filterBiggerThan20(items));
 
@@ -66,6 +78,20 @@ const items2 = [
   },
 ];
 
+// items2 = {
+// id: "Item 5"
+// category: "Category 3"
+// }
+
+function transformItems(arr) {
+  return arr.map((obj) => {
+    return {
+      id: obj.name,
+      category: obj.category.name,
+    };
+  });
+}
+
 console.log(transformItems(items2));
 
 // Reduce
@@ -73,17 +99,49 @@ console.log(transformItems(items2));
 // Use reduce method to group all items by category, the result should be an object where they key is
 // the name of the category and the value is an array of all matching items for that category
 
+function groupByCategory(items2) {
+  return items2.reduce((acc, item) => {
+    return {
+      ...acc,
+      [item.category.name]: [...(acc[item.category.name] || []), item],
+    };
+  }, {});
+}
+
 console.log(groupByCategory(items2));
 
 // Mapping (Dictionary)
 // Use the following dictionary to decode the secret message
-const dict = { x: "c", o: "o", b: "d", r: "e", n: "i", y: 's', p: 'l', a: 'f', w: 'a', u: 'w', c: 'm'};
+const dict = {
+  x: "c",
+  o: "o",
+  b: "d",
+  r: "e",
+  n: "i",
+  y: "s",
+  p: "l",
+  a: "f",
+  w: "a",
+  u: "w",
+  c: "m",
+};
 
+function decodeMessage(message, dictionary) {
+  const chars = message.split("");
+  return chars
+    .map((char) => {
+      if (char === " ") {
+        return char;
+      }
+      return dictionary[char];
+    })
+    .join("");
+}
 
-console(decodeMessage("xobr ny pnar xobr ny wuryocy", dict))
+console.log(decodeMessage("xobr ny pnar xobr ny wuryocy", dict));
 
 // Count words lower case
-// Count all independent workds in the following test
+// Count all independent words in the following test
 
 const text1 = `
 Augusta Ada King, Countess of Lovelace (née Byron; 10 December 1815 - 27 November 1852) was an English mathematician and writer, chiefly known for her work on Charles Babbage's proposed mechanical general-purpose computer, the Analytical Engine.
@@ -98,8 +156,24 @@ Upon her death, she was buried next to him at her request. Although often ill in
 She married William King in 1835. King was made Earl of Lovelace in 1838, Ada thereby becoming Countess of Lovelace.
 `;
 
+function countWords(text) {
+  const words = text.toLowerCase().split(" ");
+  return words.reduce((acc, word) => {
+    return { ...acc, [word]: (acc[word] || 0) + 1 };
+  }, {});
+}
 console.log(countWords(text1));
 // Count letters lowercase
 // Create a function that counts all the letters in the text provided
+
+function countLetters(text) {
+  const characters = text.toLowerCase().split("");
+  return characters.reduce((acc, character) => {
+    return {
+      ...acc,
+      [character]: (acc[character] || 0) + 1,
+    };
+  }, {});
+}
 
 console.log(countLetters(text1));
