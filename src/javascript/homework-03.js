816b3e593946f2bff93c43d7a1412ecef4a2d204
mjvemo <74282064+mjvemo@users.noreply.github.com>
2024-02-23 23:20:41 -0600

    Review Homework-03
    Co-authored-by: Daniel Solís Méndez <dsolisqcells@users.noreply.github.com>

--- src/javascript/homework-03.js
+++ src/javascript/homework-03.js
@@ -49,5 +49,432 @@ const products1 = [
 // Implement both using for loops and array methods
 
 // 1. Create a function that returns an object with all the products with the highest amount for the combination storeId and productId
+
+/**
+ * {
+ * store-1 product-1 amount: 3-7,
+ * store-2 product-1 amount: 5
+ * store-2 product-2 amount: 1-9-3
+ * store-3 product-1 amount: 2-5
+ * store-2 product-3 amount: 6
+ * }
+ */
+
+const obj1 = {
+  hello: 1,
+  "product-1": {
+    product: 1,
+    value: 2,
+  },
+  "product-2": {
+    product: 2,
+    value: 3,
+  },
+  "{abc: 1}": {
+    amount: 1,
+  },
+};
+
+// obj['hello'] => 1
+
+// Object keys are ALWAYS strings
+// obj['product-1'].value;
+
+// obj[{klamsd}].value
+// obj[{abc: 1}]
+
+function productsHighestAmount(arr) {
+  let acc = {};
+  // acc => {}
+  // acc => {{storeId: 'store-1', productId: 'product-1', amount: 7}, {{storeId: 'store-2', productId: 'product-1', amount: 5} {storeId: 'store-2', productId: 'product-2', amount: 9}, storeId: 'store-3', productId: 'product-1', amount: 5}, storeId: 'store-2', productId: 'product-3', amount: 6
+
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i]; // [{1 object}, {2 object}, {3 object}]
+    let key = `${item.storeId}~${item.productId}`;
+    let elem = acc[key]; // {store: store-1, product: produc-1, amount: 1}
+
+    if (elem) {
+      if (item.amount > elem.amount) {
+        acc[key] = item;
+      }
+    } else {
+      // acc['store-1~product-1'] = {store: store-1, product: produc-1, amount: 1}
+      // acc = {'store-1~product-1': {store: store-1, product: produc-1, amount: 1}}
+      // acc['store-1~product-1'] => {store: store-1, product: produc-1, amount: 1}
+      acc[key] = item;
+    }
+
+    // const amount = acc.amount > item.amount ? acc[item].amount : item.amount;
+
+    // acc = {
+    //   ...acc,
+
+    //   storeId: item.storeId,
+    //   productId: item.productId,
+    //   amount: amount,
+    // };
+  }
+  return acc;
+}
+
+console.log("============ product highest amount =========");
+console.log(productsHighestAmount(products1));
+
+function highestAmountObj(arr) {
+  return arr.reduce((acc, item) => {
+    let key = `${item.storeId}~${item.productId}`;
+    const elem = acc[key];
+
+    // if (elem) {
+    //   if (item.amount > elem.amount) {
+    //     acc[key] = item;
+    //   }
+    // } else {
+    //   acc[key] = item;
+    // }
+
+    // acc[key] = elem ? (item.amount > elem.amount ? item : elem) : item;
+    const final = {
+      ...item,
+      amount: elem ? Math.max(item.amount, elem.amount) : item.amount,
+    };
+
+    return {
+      ...acc,
+      [key]: final,
+    };
+
+    // const amount =
+    //   acc[item] && acc[item].amount > item.amount
+    //     ? acc[item].amount
+    //     : item.amount;
+
+    // return acc;
+  }, {});
+}
+
+console.log("========= highest amount obj");
+console.log(highestAmountObj(products1));
+
+const products2 = [
+  {
+    storeId: "store-1",
+    productId: "product-1",
+    userId: "user-1",
+    discount: [1, 20, 4],
+  }, // => [{storeId: '', productId: ''}, {storeId: '', productId: ''}, {storeId: '', productId: ''}]
+  {
+    storeId: "store-2",
+    productId: "product-1",
+    userId: "user-2",
+    amount: 5,
+    discount: [10, 2, 4],
+  },
+  {
+    storeId: "store-1",
+    productId: "product-1",
+    userId: "user-1",
+    amount: 7,
+    discount: [1, 2, 40],
+  }, // => [{storeId: '', productId: ''}, {storeId: '', productId: ''}, {storeId: '', productId: ''}, ...7 more]
+  {
+    storeId: "store-2",
+    productId: "product-2",
+    userId: "user-2",
+    amount: 1,
+    discount: [11, 2, 4],
+  },
+  {
+    storeId: "store-3",
+    productId: "product-1",
+    userId: "user-2",
+    amount: 2,
+    discount: [1, 22, 4],
+  },
+  {
+    storeId: "store-2",
+    productId: "product-2",
+    userId: "user-1",
+    amount: 9,
+    discount: [1, 25, 4],
+  },
+  {
+    storeId: "store-3",
+    productId: "product-1",
+    userId: "user-3",
+    amount: 5,
+    discount: [1, 2, 41],
+  },
+  {
+    storeId: "store-2",
+    productId: "product-3",
+    userId: "user-1",
+    amount: 6,
+    discount: [19, 2, 4],
+  },
+  {
+    storeId: "store-2",
+    productId: "product-2",
+    userId: "user-2",
+    amount: 3,
+    discount: [1, 2, 14],
+  },
+  {
+    storeId: "store-1",
+    productId: "product-1",
+    userId: "user-1",
+    amount: 3,
+    discount: [1, 26, 4],
+  }, // => [{storeId: '', productId: ''}, {storeId: '', productId: ''}, {storeId: '', productId: ''}]
+  {
+    storeId: "store-2",
+    productId: "product-1",
+    userId: "user-3",
+    amount: 5,
+    discount: [1, 2, 54],
+  },
+  {
+    storeId: "store-1",
+    productId: "product-1",
+    userId: "user-3",
+    amount: 7,
+    discount: [1, 28, 4],
+  }, // => [{storeId: '', productId: ''}, {storeId: '', productId: ''}, {storeId: '', productId: ''}, ...7 more]
+  {
+    storeId: "store-2",
+    productId: "product-2",
+    userId: "user-1",
+    amount: 1,
+    discount: [19, 2, 4],
+  },
+  {
+    storeId: "store-3",
+    productId: "product-1",
+    userId: "user-1",
+    amount: 2,
+    discount: [1, 62, 4],
+  },
+  {
+    storeId: "store-2",
+    productId: "product-2",
+    userId: "user-2",
+    amount: 9,
+    discount: [71, 2, 4],
+  },
+  {
+    storeId: "store-3",
+    productId: "product-1",
+    userId: "user-2",
+    amount: 5,
+    discount: [1, 12, 4],
+  },
+  {
+    storeId: "store-2",
+    productId: "product-3",
+    userId: "user-1",
+    amount: 6,
+    discount: [1, 42, 4],
+  },
+  {
+    storeId: "store-2",
+    productId: "product-2",
+    userId: "user-3",
+    amount: 3,
+    discount: [1, 52, 4],
+  },
+];
+
+// List of users in every store, they have being accumulating discounts per each purchase, create a object with the combination of store, product and user
+// with the highest discount for the user, update the shape of the object where the discount is a number with the max discount instead of an array
+// output should be an object were the key is store, product, user combination and the values are the product with highest discount
+
+function findMax(arr) {
+  return arr.reduce((acc, max) => {
+    return acc > max ? acc : max;
+  });
+}
+
+function highestDiscount(arr) {
+  return arr.reduce((acc, obj) => {
+    let key = `${obj.storeId}~${obj.productId}~${obj.userId}`;
+
+    // const elem = acc[key] => undefined
+
+    const elem = acc[key];
+    // { store, product, user, discount: number }
+
+    // const discount = [...obj.discount, ...(elem ? [elem.discount] : [])].reduce(
+    //   (max, num) => (max > num ? max : num)
+    // );
+
+    if (elem) {
+      acc[key] = {
+        ...obj,
+        discount: findMax([...obj.discount, elem.discount]),
+      };
+    } else {
+      acc[key] = { ...obj, discount: findMax(obj.discount) };
+    }
+
+    // return {
+    //   ...acc,
+    //   [key]: {
+    //     ...obj,
+    //     discount,
+    //   },
+    // };
+    return acc;
+  }, {});
+}
+
+// Object.keys(obj);
+
+console.log(highestDiscount(products2));
+
+// Return and object with all discounts available for the combination of store, product and user
+
+function mergeDiscount(arr) {
+  return arr.reduce((acc, obj) => {
+    let key = `${obj.storeId}~${obj.productId}~${obj.userId}`;
+    const elem = acc[key];
+
+    if (elem) {
+      acc[key] = { ...obj, discount: [...obj.discount, ...elem.discount] };
+    } else {
+      acc[key] = obj;
+    }
+
+    // acc[key] = elem ? (acc[key] = [...obj, ...obj.discount]) : (acc[key] = obj);
+    // return {
+    //   ...acc,
+    //   [key]: {
+    //     ...obj,
+    //   }
+    // }
+    return acc;
+  }, {});
+}
+console.log(mergeDiscount(products2));
+
+// Sum all discounts for combination store,
+// product, user => {'': {store, product, user, discount: 250}}
+// => elem = {store, product, user, discount: 250}
+
+function sumDiscount(arr) {
+  return arr.reduce((acc, obj) => {
+    let key = `${obj.storeId}~${obj.productId}~${obj.userId}`;
+    const elem = acc[key];
+    const sum = obj.discount.reduce((sum, num) => sum + num);
+
+    if (elem) {
+      acc[key] = {
+        ...obj,
+        discount: sum + elem.discount,
+      };
+    } else {
+      acc[key] = {
+        ...obj,
+        discount: sum,
+      };
+    }
+    return acc;
+  }, {});
+}
+
+console.log(sumDiscount(products2));
+
+// Assuming all products discounts have always the same length, sum all the discounts in the same position of the array
+// say you have 3 products with the following discounts
+// [1, 3, 5, 4]
+// [3, 4, 7, 6]
+// [1, 6, 9, 3]
+// ---
+// [5, 13, 21, 13]
+
+// NO NO
+// [0] + [0]
+// [1] + [1]
+// [2] + [2]
+
+// return an object with the combination store, product, user with the resulting discounts array as follows
+// {
+//    'store-1~product-1~user-1': {
+//         storeId: 'store-1',
+//         productId: 'product-1',
+//         userId: 'user-1',
+//         discount: [5, 13, 21]
+//     },
+//    'store-2~product-2~user-1': { ... }
+// }
+
 // 2. Create a function that multiplies/generates and accumulates products in an array for the combination of storeId and productId,
 //    the end result is an object where the keys are the composite key of storeId and productId and the value is the array with all generated products
+// => amount = 3 => [{},{},{}]
+// => [{storeId: '', productId: ''}, {storeId: '', productId: ''}, {storeId: '', productId: ''}, ...7 more]
+
+function generateProductsObj(arr) {
+  let acc = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+
+    acc = item;
+  }
+  return acc;
+}
+
+console.log("======== products obj =========");
+console.log(generateProductsObj(products1));
+
+function generateObjByLength(arr) {
+  // new Array(10).fill({});
+
+  // let count = product.amount;
+  // const generated = [];
+  // while(count--) { 0
+  //   generate.push(product);
+  // }
+
+  // for (let i = product.amount; i > 0; i--) {
+  //  generated.push(product);
+  // }
+  //
+
+  // {
+  //   'store-1~product-1': [{store, product, ...rest}, {store, product, ...rest}, {store, product, ...rest}, ...n more],
+  //   'store-2~product-1': [{}, {}, {}, {}]
+  // }
+
+  // {store, product, amount: 3}
+  // {store, product, amount: 5}
+  // { 'store-1~product-1': [{store, product, ...rest}, ...7 more] }
+  return arr.reduce((acc, product) => {
+    let generated = [];
+
+    let key = `${product.storeId}~${product.productId}`;
+
+    // acc[key] => {}['store-1~product-1'] => [] | undefined
+    const products = acc[key];
+
+    for (let i = product.amount; i > 0; i--) {
+      generated.push(product);
+    }
+
+    return {
+      ...acc,
+      [key]: products ? [...products, ...generated] : [...generated],
+    };
+  }, {});
+}
+
+// arr
+//   .reduce((acc, item) => {
+//     let amount = acc[item].amount > item.amount ? acc : item;
+//     if (amount) {
+//       return { ...acc, storeId: item.storeId, products1: item.productId };
+//     }
+//   }, [])
+//   .map((item) => item.amount.length);
+
+console.log("========== by length ========");
+console.log(generateObjByLength(products1));
--- src/javascript/homework-04.mjs
+++ src/javascript/homework-04.mjs
@@ -2,6 +2,8 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:3000";
+const instance = axios.create({ baseURL: BASE_URL });
+
 // RUN "npm run start:db" to start the database
 
 // Use axios to connect to our local database and use http method and promises to solve the following exercises
@@ -9,10 +11,112 @@ const BASE_URL = "http://localhost:3000";
 // 1. Create a product with the following attributes
 // id, name, category, amount
 
+async function createProduct(payload) {
+  const url = "/users";
+
+  const response = await instance({ url, method: "POST", data: payload });
+
+  return response.data;
+}
+
 // 2. List all products
+async function getProduct() {
+  const url = "/users";
+
+  const response = await instance({ url, method: "get" });
+
+  return response.data;
+}
 
 // 3. Update a product
 
+async function updateProduct(payload, id) {
+  const url = `/users/${id}`;
+
+  const response = await instance({ url, method: "PATCH", data: payload });
+
+  return response.data;
+}
+
 // 4. Delete a product
+async function deleteProduct(payload, id) {
+  const url = `/users/${id}`;
+
+  const response = await instance({ url, method: "DELETE", data: payload });
+
+  return response.data;
+}
 
 // 5. Create a method that list all products and updates each of the products with a new attribute called price, set the value to 0
+
+const newProducts = [
+  {
+    id: "id-01",
+    name: "stripes-shirt",
+    category: "clothes",
+    amount: 3,
+    price: 0,
+  },
+  {
+    id: "id-02",
+    name: "legant-sofa",
+    category: "furniture",
+    amount: 2,
+    price: 0,
+  },
+  {
+    id: "id-03",
+    name: "omega-figure",
+    category: "toys",
+    amount: 5,
+    price: 0,
+  },
+  {
+    id: "id-04",
+    name: "h-short",
+    category: "clothes",
+    amount: 9,
+    price: 0,
+  },
+  {
+    id: "id-05",
+    name: "cross-lamp",
+    category: "decoration",
+    amount: 7,
+    price: 0,
+  },
+];
+
+const addingPrice = await Promise.all(
+  newProducts.map((user) => {
+    // {name: '', value: 4}
+    return createProduct(user);
+  })
+);
+
+async function main() {
+  await createProduct({
+    id: "id-01",
+    name: "stripes-shirt",
+    category: "clothes",
+    amount: 3,
+  });
+  const products = await getProduct();
+  console.log(products);
+  console.log("=========== products + price ============");
+  console.log(addingPrice);
+
+  await updateProduct(
+    {
+      id: "id-04",
+      name: "h-short",
+      category: "clothes",
+      amount: 9,
+    },
+    "id-04"
+  );
+
+  await deleteProduct({ id: "id-1", name: "user-name-1", age: 90 });
+}
+
+main();
