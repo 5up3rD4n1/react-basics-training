da0a64b5ab7bafaf9511ac74335b38078681607f
mjvemo <74282064+mjvemo@users.noreply.github.com>
2024-03-05 15:07:43 -0600

    homework-test-03-mocks

--- test/unit/functions/homework-test-03-mocks.test.ts
+++ test/unit/functions/homework-test-03-mocks.test.ts
@@ -11,70 +11,112 @@ const mockInstance: jest.Mocked<UnitTestRepo> = {
 
 describe('Unit: src/typescript/functions/homework-test-03-mocks', () => {
   const subject = new MyUnitTestRepository(mockInstance);
+  const defaultResponse = {
+    data: {
+      id: 'test-id-01',
+      amount: 100,
+      category: 'pants',
+    },
+  };
 
   // test subject.save, subject.query, subject.delete
   // there is a mock implementation under the hood of the subject instance
+  beforeAll(() => {
+    mockInstance.save.mockReturnValue(defaultResponse.data.category);
+  });
+  beforeEach(() => {
+    mockInstance.save.mockClear();
+  });
 
-  describe('MyUnitTestRepository', () => {
-    describe('save', () => {
-      beforeAll(() => {
-        mockInstance.save.mockReturnValue('test-id-1');
-      });
-      beforeEach(() => {
-        mockInstance.save.mockClear();
-      });
+  // Don't know how to match the same exact value => it matches just cause it's the same type of 'string' but not for been the same value
+  describe('save', () => {
+    describe('when category is equal to shoes', () => {
+      it('returns payload and adds amount divided by 2', () => {
+        const payload = {
+          id: 'test-id-01',
+          amount: defaultResponse.data.amount,
+          category: 'shoes',
+        };
 
-      afterAll(() => {
-        mockInstance.save.mockReset();
-      });
-      describe('when category is equal to shoes', () => {
-        it('returns payload and adds amount divided by 2', () => {
-          const category = 'shoes';
-          const expectedResponse = {category: 'shoes', amount: 100 / 2};
+        const response = subject.save(payload);
 
-          const response = subject.save(expectedResponse);
-          mockInstance.save.mockReturnValue(category);
-
-          expect(mockInstance.save).toHaveBeenCalledTimes(1);
-          expect(response).toEqual(expectedResponse);
-        });
-      });
-
-      describe('when category is differen than shoes', () => {
-        it('returns the payload', () => {
-          const id = 'test-id-1';
-          const payload = {id: 'test-id-1', category: 'shorts'};
-          const response = subject.save(payload);
-          mockInstance.save.mockReturnValue(id);
-
-          expect(mockInstance.save).toHaveBeenCalledTimes(1);
-          expect(response).toEqual(payload);
-        });
+        expect(mockInstance.save).toHaveBeenCalledTimes(1);
+        expect(response).toEqual(defaultResponse.data.category);
       });
     });
 
-    describe('query', () => {});
+    describe('when category is different than shoes', () => {
+      it('returns the payload', () => {
+        const payload = {id: 'test-id-01', amount: 50, category: 'hat'};
+        const response = subject.save(payload);
 
-    describe('delete', () => {
-      beforeAll(() => {
-        mockInstance.delete.mockReturnValue('test-id-1');
+        expect(mockInstance.save).toHaveBeenCalledTimes(1);
+        expect(response).toEqual(defaultResponse.data.category);
       });
+    });
+  });
 
-      beforeEach(() => {
-        mockInstance.delete.mockClear();
+  describe('query', () => {
+    const defaultR = ['tshirt', 'pants'];
+    beforeAll(() => {
+      mockInstance.query.mockReturnValue(defaultR);
+    });
+    beforeEach(() => {
+      mockInstance.query.mockClear();
+    });
+    describe('when cateogry is tshirt', () => {
+      it('changes the category name to shirts', () => {
+        const payload = {
+          id: 'test-id-01',
+          amount: defaultResponse.data.amount,
+          category: defaultR[1],
+        };
+
+        const response = subject.save(payload);
+
+        expect(mockInstance.save).toHaveBeenCalledTimes(1);
+        expect(response).toEqual(defaultResponse.data.category);
       });
+    });
 
-      afterAll(() => {
-        mockInstance.delete.mockReset();
-      });
+    // I can't change the return value because it's set to be a string[] and its about the amount => number
+    // describe('when amount is bigger than 100', () => {
+    //   const defaultValues = ['not a number'];
+    //   beforeAll(() => {
+    //     mockInstance.query.mockReturnValue(defaultValues);
+    //   });
+    //   it('changes the amount to 100', () => {
+    //     const payload = {
+    //       id: 'test-id-01',
+    //       amount: defaultResponse.data.amount > 100,
+    //       category: defaultR[0],
+    //     };
 
-      it('sends id to repo delete method', () => {
-        const id = 'test-id-1';
-        const response = subject.delete(id);
+    //     const response = subject.save(payload);
 
-        expect(mockInstance.delete).toHaveBeenCalledTimes(1);
-        expect(response).toEqual(id);
-      });
+    //     expect(mockInstance.save).toHaveBeenCalledTimes(1);
+    //     expect(response).toEqual(defaultResponse.data.amount);
+    //   });
+    // });
+  });
+
+  describe('delete', () => {
+    beforeAll(() => {
+      mockInstance.delete.mockReturnValue('test-id-01');
+    });
+
+    it('sends id to repo delete method', () => {
+      const id = 'test-id-01';
+      const expectedResponse = {
+        id,
+        category: 'shoes',
+        amount: defaultResponse.data.amount,
+      };
+
+      const response = subject.delete(expectedResponse.id);
+
+      expect(mockInstance.delete).toBeCalledTimes(1);
+      expect(response).toEqual(expectedResponse.id);
     });
   });
 });
